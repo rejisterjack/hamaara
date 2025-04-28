7e353e9b9ac4c0c9590cbb36d7ae3e82da75b507
Rupam Das <rejisterjack@gmail.com>
2025-04-28 11:48:42 +0530

    hero

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,10 +1,28 @@
 import React from "react"
 import Navbar from "../navbar/Navbar"
+import "./hero-section.scss"
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className="hero_section">
       <Navbar />
+      <div className="hero_section_content">
+        <h1 className="hero_section_heading">
+          <span className="invest">Invest in a </span>
+          <span className="future">Brighter</span>
+          <br />
+          <span className="future">Future with </span>
+          <span className="hamaara">Hamaara</span>
+        </h1>
+        <p className="hero_section_description">
+          Join us in transforming urban living at The Planet of Hayward, where
+          modern design meets community spirit.
+        </p>
+        <div className="hero_section_buttons">
+          <button className="get-in-touch">Get in Touch</button>
+          <button className="invest">Invest</button>
+        </div>
+      </div>
     </div>
   )
 }
