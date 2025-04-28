96a06c65490fd777ba9ce89eaec7e193b53b5712
Rupam Das <rejisterjack@gmail.com>
2025-04-28 13:30:57 +0530

    images

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import NoticeBar from "./components/notice-bar/NoticeBar"
 import HeroSection from "./components/hero/HeroSection"
 import Showcase from "./components/showcase/Showcase"
+import Planet from "./components/planet/Planet"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NoticeBar />
       <HeroSection />
       <Showcase />
+      <Planet />
     </div>
   )
 }
